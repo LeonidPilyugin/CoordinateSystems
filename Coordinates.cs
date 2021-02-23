3dfb7e7b20c6eb68fe86ee7044c22dc96c9168e9
Leonid Pilyugin <l.pilyugin04@gmail.com>
2021-02-23 20:12:37 +0300

    names changed

--- Coordinates.cs
+++ Coordinates.cs
@@ -10,7 +10,7 @@ namespace CoordinateSystems
 {
     public static class Coordinates
     {
-        private static class UnmanagedCoordinates
+        public static class UnmanagedCoordinates
         {
             [DllImport("coordsystems.dll", CallingConvention = CallingConvention.Cdecl)]
             public static extern void ICStoGCS(double julianDate, ref double x, ref double y, ref double z);
@@ -40,126 +40,16 @@ namespace CoordinateSystems
             public static extern void turnZ(ref double x, ref double y, ref double z, double angle);
         }
 
-        public class Vector
-        {
-            private double x;
-            private double y;
-            private double z;
-
-            public Vector()
-            {
-                x = y = z = 0.0;
-            }
-
-            public double X
-            {
-                get { return x; }
-                set { x = value; }
-            }
-            public double Y
-            {
-                get { return y; }
-                set { y = value; }
-            }
-            public double Z
-            {
-                get { return z; }
-                set { z = value; }
-            }
-            public double length
-            {
-                get { return Sqrt(x * x + y * y + z * z); }
-                set
-                {
-                    if (value <= 0)
-                        throw new System.Exception("Value must be > 0");
-                    double t = theta;
-                    double p = phi;
-                    x = value * Sin(t) * Cos(p);
-                    y = value * Sin(t) * Sin(p);
-                    z = value * Cos(t);
-                }
-            }
-            public double theta
-            {
-                get { return Acos(z / length); }
-                set
-                {
-                    double l = length;
-                    double p = phi;
-                    x = l * Sin(value) * Cos(p);
-                    y = l * Sin(value) * Sin(p);
-                    z = l * Cos(value);
-                }
-            }
-            public double phi
-            {
-                get { return Atan(y / x); }
-                set
-                {
-                    double l = length;
-                    double t = theta;
-                    x = l * Sin(t) * Cos(value);
-                    y = l * Sin(t) * Sin(value);
-                    z = l * Cos(t);
-                }
-            }
-
-            public void turnX(double angle)
-            {
-                UnmanagedCoordinates.turnX(ref x, ref y, ref z, angle);
-            }
-            public void turnY(double angle)
-            {
-                UnmanagedCoordinates.turnY(ref x, ref y, ref z, angle);
-            }
-            public void turnZ(double angle)
-            {
-                UnmanagedCoordinates.turnZ(ref x, ref y, ref z, angle);
-            }
-            public void turnEuler(double precession, double nutation, double rotation)
-            {
-                turnZ(precession);
-                turnX(nutation);
-                turnZ(rotation);
-            }
-
-            public static Vector operator +(Vector vector1, Vector vector2)
-            {
-                Vector result = new Vector();
-                result.x = vector1.x + vector2.x;
-                result.y = vector1.y + vector2.y;
-                result.z = vector1.z + vector2.z;
-                return result;
-            }
-            public static Vector operator -(Vector vector1, Vector vector2)
-            {
-                Vector result = new Vector();
-                result.x = vector1.x - vector2.x;
-                result.y = vector1.y - vector2.y;
-                result.z = vector1.z - vector2.z;
-                return result;
-            }
-            public static Vector operator *(Vector vector1, Vector vector2)
-            {
-                Vector result = new Vector();
-                result.x = vector1.y * vector2.z - vector1.z * vector2.y;
-                result.y = vector1.z * vector2.x - vector1.x * vector2.z;
-                result.z = vector1.x * vector2.y - vector1.y * vector2.x;
-                return result;
-            }
-        }
-
         public class Date
         {
             public struct Calendar
             {
-                public int year;
-                public int month;
-                public int day;
-                public int hour;
-                public int minute;
-                public double second;
+                public int Year;
+                public int Month;
+                public int Day;
+                public int Hour;
+                public int Minute;
+                public double Second;
             }
 
             private double julianDate;
@@ -171,33 +61,33 @@ namespace CoordinateSystems
             }
             public Date(Calendar calendar)
             {
-                julianDate = getJulianDate(calendar);
+                julianDate = GetJulianDate(calendar);
             }
             public Date(int year, int month, int day, int hour, int minute, double second)
             {
                 Calendar calendar;
-                calendar.year = year;
-                calendar.month = month;
-                calendar.day = day;
-                calendar.hour = hour;
-                calendar.minute = minute;
-                calendar.second = second;
-                julianDate = getJulianDate(calendar);
+                calendar.Year = year;
+                calendar.Month = month;
+                calendar.Day = day;
+                calendar.Hour = hour;
+                calendar.Minute = minute;
+                calendar.Second = second;
+                julianDate = GetJulianDate(calendar);
             }
             public Date(double julianDate)
             {
                 this.julianDate = julianDate;
             }
 
-            private double getJulianDate(Calendar calendar)
+            private double GetJulianDate(Calendar calendar)
             {
-                int a = (14 - calendar.month) / 12;
-                int y = calendar.year + 4800 - a;
-                int m = calendar.month + 12 * a - 3;
-                return calendar.day + (int)((153 * m + 2) / 5) + 365 * y +
+                int a = (14 - calendar.Month) / 12;
+                int y = calendar.Year + 4800 - a;
+                int m = calendar.Month + 12 * a - 3;
+                return calendar.Day + (int)((153 * m + 2) / 5) + 365 * y +
                     (int)(y / 4) - (int)(y / 100) + (int)(y / 400) - 32045;
             }
-            private Calendar getGrigorianCalendar(double julianDate)
+            private Calendar GetGrigorianCalendar(double julianDate)
             {
                 int a = (int)julianDate + 32044;
                 int b = (4 * a + 3) / 146097;
@@ -206,32 +96,32 @@ namespace CoordinateSystems
                 int e = c - 1461 * d / 4;
                 int m = (5 * e + 2) / 153;
                 Calendar calendar;
-                calendar.day = e - (153 * m + 2) / 5 + 1;
-                calendar.month = m + 3 - 12 * (m / 10);
-                calendar.year = 100 * b + d - 4800 + m / 10;
+                calendar.Day = e - (153 * m + 2) / 5 + 1;
+                calendar.Month = m + 3 - 12 * (m / 10);
+                calendar.Year = 100 * b + d - 4800 + m / 10;
                 double time = julianDate - (int)julianDate;
                 time *= 24;
-                calendar.hour = (int)(time);
-                time = (time - calendar.hour) * 60;
-                calendar.minute = (int)(time);
-                calendar.second = (time - calendar.minute) * 60;
+                calendar.Hour = (int)(time);
+                time = (time - calendar.Hour) * 60;
+                calendar.Minute = (int)(time);
+                calendar.Second = (time - calendar.Minute) * 60;
                 return calendar;
             }
-            private bool isDayCorrect(int value)
+            private bool IsDayCorrect(int value)
             {
                 if (value < 1)
                     return false;
 
-                int month = GrigorianCalendar.month;
+                int month = GrigorianCalendar.Month;
                 if (month % 2 == 1 && month < 8)
                     return value < 32;
                 if (month % 2 == 1)
                     return value < 31;
 
-                int year = GrigorianCalendar.year;
+                int year = GrigorianCalendar.Year;
                 if (month == 2)
                 {
-                    if (isLeap())
+                    if (IsLeap())
                         return value < 30;
                     else
                         return value < 29;
@@ -245,21 +135,21 @@ namespace CoordinateSystems
 
             public Calendar GrigorianCalendar
             {
-                get { return getGrigorianCalendar(julianDate); }
+                get { return GetGrigorianCalendar(julianDate); }
                 set
                 {
-                    if (value.second < 0.0 || value.second >= 60.0)
+                    if (value.Second < 0.0 || value.Second >= 60.0)
                         throw new System.Exception("second isn't correct");
-                    if (value.minute < 0 || value.minute > 59)
+                    if (value.Minute < 0 || value.Minute > 59)
                         throw new System.Exception("minute isn't correct");
-                    if (value.hour < 0 || value.hour > 23)
+                    if (value.Hour < 0 || value.Hour > 23)
                         throw new System.Exception("hour isn't correct");
-                    if (value.day < 0 || value.day > 31)
+                    if (value.Day < 0 || value.Day > 31)
                         throw new System.Exception("day isn't correct");
-                    if (value.month < 0 || value.month > 12)
+                    if (value.Month < 0 || value.Month > 12)
                         throw new System.Exception("month isn't correct");
 
-                    julianDate = getJulianDate(value);
+                    julianDate = GetJulianDate(value);
                 }
             }
             public double JulianDate
@@ -274,71 +164,71 @@ namespace CoordinateSystems
             }
             public double Second
             {
-                get { return GrigorianCalendar.second; }
+                get { return GrigorianCalendar.Second; }
                 set
                 {
                     if (value < 0.0 || value >= 60.0)
                         throw new System.Exception("second isn't correct");
                     Calendar calendar = GrigorianCalendar;
-                    calendar.second = value;
+                    calendar.Second = value;
                     GrigorianCalendar = calendar;
                 }
             }
             public int Minute
             {
-                get { return GrigorianCalendar.minute; }
+                get { return GrigorianCalendar.Minute; }
                 set
                 {
                     if (value < 0 || value > 59)
                         throw new System.Exception("minute isn't correct");
                     Calendar calendar = GrigorianCalendar;
-                    calendar.minute = value;
+                    calendar.Minute = value;
                     GrigorianCalendar = calendar;
                 }
             }
             public int Hour
             {
-                get { return GrigorianCalendar.hour; }
+                get { return GrigorianCalendar.Hour; }
                 set
                 {
                     if (value < 0 || value > 23)
                         throw new System.Exception("hour isn't correct");
                     Calendar calendar = GrigorianCalendar;
-                    calendar.hour = value;
+                    calendar.Hour = value;
                     GrigorianCalendar = calendar;
                 }
             }
             public int Day
             {
-                get { return GrigorianCalendar.day; }
+                get { return GrigorianCalendar.Day; }
                 set
                 {
-                    if (!isDayCorrect(value))
+                    if (!IsDayCorrect(value))
                         throw new System.Exception("day isn't correct");
                     Calendar calendar = GrigorianCalendar;
-                    calendar.day = value;
+                    calendar.Day = value;
                     GrigorianCalendar = calendar;
                 }
             }
             public int Month
             {
-                get { return GrigorianCalendar.month; }
+                get { return GrigorianCalendar.Month; }
                 set
                 {
                     if (value < 1 || value > 12)
                         throw new System.Exception("month isn't correct");
                     Calendar calendar = GrigorianCalendar;
-                    calendar.month = value;
+                    calendar.Month = value;
                     GrigorianCalendar = calendar;
                 }
             }
             public int Year
             {
-                get { return GrigorianCalendar.year; }
+                get { return GrigorianCalendar.Year; }
                 set
                 {
                     Calendar calendar = GrigorianCalendar;
-                    calendar.year = value;
+                    calendar.Year = value;
                     GrigorianCalendar = calendar;
                 }
             }
@@ -356,9 +246,9 @@ namespace CoordinateSystems
                 return result;
             }
 
-            public bool isLeap()
+            public bool IsLeap()
             {
-                int year = GrigorianCalendar.year;
+                int year = GrigorianCalendar.Year;
                 if (year % 400 == 0)
                     return true;
                 if (year % 100 == 0)
@@ -461,6 +351,117 @@ namespace CoordinateSystems
             result.Y = y;
             result.Z = z;
 
+            return result;
+        }
+    }
+
+
+    public class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector()
+        {
+            x = y = z = 0.0;
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public double Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+        public double Length
+        {
+            get { return Sqrt(x * x + y * y + z * z); }
+            set
+            {
+                if (value <= 0)
+                    throw new System.Exception("Value must be > 0");
+                double t = Theta;
+                double p = Phi;
+                x = value * Sin(t) * Cos(p);
+                y = value * Sin(t) * Sin(p);
+                z = value * Cos(t);
+            }
+        }
+        public double Theta
+        {
+            get { return Acos(z / Length); }
+            set
+            {
+                double l = Length;
+                double p = Phi;
+                x = l * Sin(value) * Cos(p);
+                y = l * Sin(value) * Sin(p);
+                z = l * Cos(value);
+            }
+        }
+        public double Phi
+        {
+            get { return Atan(y / x); }
+            set
+            {
+                double l = Length;
+                double t = Theta;
+                x = l * Sin(t) * Cos(value);
+                y = l * Sin(t) * Sin(value);
+                z = l * Cos(t);
+            }
+        }
+
+        public void TurnX(double angle)
+        {
+            Coordinates.UnmanagedCoordinates.turnX(ref x, ref y, ref z, angle);
+        }
+        public void TurnY(double angle)
+        {
+            Coordinates.UnmanagedCoordinates.turnY(ref x, ref y, ref z, angle);
+        }
+        public void TurnZ(double angle)
+        {
+            Coordinates.UnmanagedCoordinates.turnZ(ref x, ref y, ref z, angle);
+        }
+        public void TurnEuler(double precession, double nutation, double rotation)
+        {
+            TurnZ(precession);
+            TurnX(nutation);
+            TurnZ(rotation);
+        }
+
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            Vector result = new Vector();
+            result.x = vector1.x + vector2.x;
+            result.y = vector1.y + vector2.y;
+            result.z = vector1.z + vector2.z;
+            return result;
+        }
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            Vector result = new Vector();
+            result.x = vector1.x - vector2.x;
+            result.y = vector1.y - vector2.y;
+            result.z = vector1.z - vector2.z;
+            return result;
+        }
+        public static Vector operator *(Vector vector1, Vector vector2)
+        {
+            Vector result = new Vector();
+            result.x = vector1.y * vector2.z - vector1.z * vector2.y;
+            result.y = vector1.z * vector2.x - vector1.x * vector2.z;
+            result.z = vector1.x * vector2.y - vector1.y * vector2.x;
             return result;
         }
     }
